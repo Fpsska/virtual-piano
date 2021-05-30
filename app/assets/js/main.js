319acd60cab24ab3907896ef5aebccdcef7109dd
Fpsska <fpsska1337@gmail.com>
2021-05-30 14:21:59 +0300

    feat: add responsive for all resolutions

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,122 +1,128 @@
-const piano = document.querySelector(".piano");
-const pianoKeys = document.querySelectorAll(".piano__key");
+document.addEventListener("DOMContentLoaded", () => {
 
-const buttons = document.querySelectorAll(".btn");
-const notesBtn = document.querySelector(".btn--notes");
-const lettersBtn = document.querySelector(".btn--letters");
+    let piano = document.querySelector(".piano");
+    let pianoKeys = document.querySelectorAll(".piano__key");
+
+    let buttons = document.querySelectorAll(".btn");
+    let notesBtn = document.querySelector(".btn--notes");
+    let lettersBtn = document.querySelector(".btn--letters");
+
+    notesBtn.classList.add("active")
+
+    buttons.forEach(item => {
+
+        item.addEventListener("mousedown", () => {   // btn active
+            const currentBtn = item;
+
+            buttons.forEach(item => {
+                item.classList.remove("active");
+            });
+            currentBtn.classList.add("active");
 
 
-buttons.forEach(item => {
-
-    item.addEventListener("mousedown", () => {   // btn active
-        const currentBtn = item;
-
-        buttons.forEach(item => {
-            item.classList.remove("active");
+            if (lettersBtn.classList.contains("active")) {   // switch attr 
+                pianoKeys.forEach(item => {
+                    item.classList.add("letter");
+                });
+            } else if (notesBtn.classList.contains("active")) {
+                pianoKeys.forEach(item => {
+                    item.classList.remove("letter");
+                });
+            }
         });
-        currentBtn.classList.add("active");
 
-
-        if (lettersBtn.classList.contains("active")) {   // switch attr 
-            pianoKeys.forEach(item => {
-                item.classList.add("letter");
-            });
-        } else if (notesBtn.classList.contains("active")) {
-            pianoKeys.forEach(item => {
-                item.classList.remove("letter");
-            });
-        }
     });
 
-});
+    // /.BTN FUNCTION
 
-// /.BTN FUNCTION
+    let playAudio = (src) => {
+        if (src != undefined) {
+            const audio = new Audio();
+            audio.src = src;
+            audio.currentTime = 0;
+            audio.play();
+        }
+    };
 
-let playAudio = (src) => {
-    if (src != undefined) {
-        const audio = new Audio();
-        audio.src = src;
-        audio.currentTime = 0;
-        audio.play();
-    }
-};
-
-piano.addEventListener("click", (event) => {
-    if (event.target.classList.contains("piano__key")) {
-        const note = event.target.dataset.note;
-        const src = `assets/audio/${note}.mp3`;
-        playAudio(src);
-    }
-});
-
-const keyTrack = (event) => {
-
-    pianoKeys.forEach((el) => {
-        if (el.dataset.letter == event[3]) {
-            const note = el.dataset.note;
+    piano.addEventListener("click", (event) => {
+        if (event.target.classList.contains("piano__key")) {
+            const note = event.target.dataset.note;
             const src = `assets/audio/${note}.mp3`;
             playAudio(src);
-            el.classList.add('active');
         }
     });
 
-};
+    const keyTrack = (event) => {
 
-const keyTrackOff = () => {
-    pianoKeys.forEach(el => {
-        el.classList.remove("active");
+        pianoKeys.forEach((el) => {
+            if (el.dataset.letter == event[3]) {
+                const note = el.dataset.note;
+                const src = `assets/audio/${note}.mp3`;
+                playAudio(src);
+                el.classList.add('active');
+            }
+        });
+
+    };
+
+    const keyTrackOff = () => {
+        pianoKeys.forEach(el => {
+            el.classList.remove("active");
+        });
+    };
+
+    window.addEventListener('keydown', (event) => {
+        keyTrack(event.code);
     });
-};
+    window.addEventListener('keyup', keyTrackOff);
 
-window.addEventListener('keydown', (event) => {
-    keyTrack(event.code);
+
+    // /.AUDIO FUNCTION
+
+    let fullBtn = document.querySelectorAll(".btn--fullscreen");
+
+    fullBtn.forEach(item => {
+        item.addEventListener("click", () => {
+            item.classList.toggle("active")
+            activateFullscreen(document.documentElement);
+
+            if (!item.classList.contains("active")) {
+                deactivateFullscreen(document.documentElement);
+            }
+        })
+
+        item.addEventListener("keydown", event => {
+            if (item.classList.contains("active") && event.code === "Escape") {
+                item.classList.remove("active")
+                deactivateFullscreen(document.documentElement);
+            }
+        })
+    })
+
+
+
+
+
+    function activateFullscreen(element) {
+        if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        }
+        else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        }
+        else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+
+    function deactivateFullscreen() {
+        if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+
+    // /.API FULLSCREEN
+
 });
-window.addEventListener('keyup', keyTrackOff);
-
-
-// /.AUDIO FUNCTION
-
-const fullBtn = document.querySelector(".btn--fullscreen");
-
-fullBtn.addEventListener("click", () => {
-    activateFullscreen(document.documentElement);
-    deactivateFullscreen(document.documentElement);
-    fullBtn.classList.toggle("active");
-});
-
-fullBtn.addEventListener('keydown', (event) => {
-    if (event.code === "Escape") {
-        fullBtn.classList.remove("active");
-    }
-});
-
-function activateFullscreen(element) {
-    if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    }
-    else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-    }
-    else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-    }
-}
-
-function deactivateFullscreen() {
-    if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    }
-}
-// /.API FULLSCREEN
-
-
-
-
-
-
-
-
-
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 const { src, dest, watch, parallel, series } = require("gulp");
 
 const scss = require("gulp-sass"); // минификация
-const concat = require("gulp-concat"); // конкатенация + имя
+const concat = require("gulp-concat"); // конкатенация/объединение + имя
 const browserSync = require("browser-sync").create(); // live update 
-const uglify = require("gulp-uglify-es").default; //
+const uglify = require("gulp-uglify-es").default;
 const autoprefixer = require("gulp-autoprefixer");
 const imagemin = require("gulp-imagemin"); // отпимизация img
 const del = require("del"); // удаление dist-папки
-
 const ghpages = require('gh-pages'); // gh-pages for dist folder
 
 
@@ -24,40 +23,6 @@ function browsersync() {  // live update
     });
 }
 
-// function watching() { 
-//     watch(["app/scss/**/*.scss"], styles);
-//     watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
-//     watch(["app/*.html"]).on("change", browserSync.reload);
-// }
-function watching() {
-    watch(["app/assets/scss/**/*.scss"], styles);
-    watch(["app/assets/js/**/*.js", "!app/assets/js/main.min.js"], scripts);
-    watch(["app/*.html"]).on("change", browserSync.reload);
-}
-
-// function build() {
-//    return src([
-//         "app/*.html",
-//         "app/fonts/**/*",
-//         "app/css/style.min.css",
-//         "app/js/main.min.js" 
-//     ], { base: "app" })
-//         .pipe(dest("dist"));
-// }
-function build() {
-    return src([
-        "app/*.html",
-        "app/assets/css/style.css",
-        "app/assets/js/main.js"
-    ], { base: "app" })
-        .pipe(dest([
-            "dist"
-        ]));
-}
-
-function cleanDist() {
-    return del("dist");
-}
 
 function images() {
     return src("app/assets/img/**/*")
@@ -79,28 +44,41 @@ function images() {
 function scripts() {
     return src([
         "node_modules/jquery/dist/jquery.js",
-        "app/js/main.js"
+        "app/assets/js/main.js"
     ])
         .pipe(concat("main.min.js")) // конкатенация + единое название 
-        .pipe(uglify())   // минификация
-        .pipe(dest("app/js"))  // конечный путь
+        // .pipe(uglify())   // минификация
+        .pipe(dest("app/assets/js"))  // конечный путь
         .pipe(browserSync.stream());
 }
 
 function styles() {  /*КОМПИЛЯЦИЯ scss -> style.min.css*/
-    return src("app/assets/js/main.js")
-    .pipe(scss())
-    .pipe(dest("app/css"));
-        // .pipe(scss({ outputStyle: "compressed" })) // минификация
-        // .pipe(concat("style.css")) // конкатенация + единое название  
-        // .pipe(autoprefixer({
-        //     overrideBrowserslist: ["last 10 version"],
-        //     grid: true
-        // }))
-        // .pipe(dest("app/css")) // выкидывает в app/css
-        // .pipe(browserSync.stream());
+    return src("app/assets/scss/style.scss")
+        .pipe(scss({ outputStyle: "expanded" }))
+        .pipe(concat("style.min.css"))
+        .pipe(dest("app/css"));
 }
 
+function watching() {
+    watch(["app/assets/scss/**/*.scss"], styles);
+    watch(["app/assets/js/**/*.js", "!app/assets/js/main.min.js"], scripts);
+    watch(["app/*.html"]).on("change", browserSync.reload);
+}
+
+function build() {
+    return src([
+        "app/*.html",
+        "app/css/style.min.css",
+        "app/assets/js/main.min.js"
+    ], { base: "app" })
+        .pipe(dest([
+            "dist"
+        ]));
+}
+
+function cleanDist() {
+    return del("dist");
+}
 
 exports.styles = styles;
 exports.watching = watching;
